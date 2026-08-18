6769d80bb598b45283ee5a0b8e4ddf3d803aa5b9
brightcat <geokyll.14@gmail.com>
2026-08-18 06:44:03 +0300

    shell script for quick training

---
+++ scripts/weak-hardware-train.sh
@@ -0,0 +1,1 @@
+python3 scripts/train.py --epochs 6 --batch_size 16 --max_train_rows None --max_length auto --save_strategy steps --save_steps 20000 --eval_steps 5000 --eval_strategy steps --resume auto
